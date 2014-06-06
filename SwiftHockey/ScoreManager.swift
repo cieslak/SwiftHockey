ccb2f49d75d87d0d625af674dc3df28944fa41bb
Chris Cieslak <chris@electropuf.com>
2014-06-05 19:32:23 -0500

    Moved some code around

--- SwiftHockey/ScoreManager.swift
+++ SwiftHockey/ScoreManager.swift
@@ -23,9 +23,9 @@ class ScoreManager: NSObject, NSURLSessionDelegate {
         }
     }
     
-    //let apiBaseURL = NSURL(string:"https://api.hockeystreams.com/Scores?key=7344b5c9c89372d26b068022c9f28175&date=03/14/2014")
+    let apiBaseURL = NSURL(string:"https://api.hockeystreams.com/Scores?key=7344b5c9c89372d26b068022c9f28175&date=03/14/2014")
 
-    let apiBaseURL = NSURL(string:"https://api.hockeystreams.com/Scores?key=7344b5c9c89372d26b068022c9f28175")
+    //let apiBaseURL = NSURL(string:"https://api.hockeystreams.com/Scores?key=7344b5c9c89372d26b068022c9f28175")
 
     let urlSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: nil, delegateQueue: nil)
     
@@ -35,60 +35,6 @@ class ScoreManager: NSObject, NSURLSessionDelegate {
         let request = NSMutableURLRequest(URL: apiBaseURL)
         let task = urlSession.downloadTaskWithRequest(request, completionHandler: {
             (url, response, error) in
-
-            func generateGame(jsonDict: NSDictionary) -> Game? {
-                
-                let leagueString: String? = jsonDict["event"] as? String
-                if leagueString == nil {
-                    return nil
-                }
-                let league = League.fromRaw(leagueString!)
-
-                let awayTeamCity: String? = jsonDict["awayTeamCity"] as? String
-                let awayTeamName: String? = jsonDict["awayTeamName"] as? String
-                let shortAwayTeam: String? = jsonDict["shortAwayTeam"] as? String
-                if awayTeamCity == nil || awayTeamName == nil ||  shortAwayTeam == nil {
-                    return nil
-                }
-                let awayTeam = Team(cityName: awayTeamCity!, teamName: awayTeamName!, shortName: shortAwayTeam!.uppercaseString)
-            
-                let homeTeamCity: String? = jsonDict["homeTeamCity"] as? String
-                let homeTeamName: String? = jsonDict["homeTeamName"] as? String
-                let shortHomeTeam: String? = jsonDict["shortHomeTeam"] as? String
-                if homeTeamCity == nil || homeTeamName == nil ||  shortHomeTeam == nil {
-                    return nil
-                }
-                let homeTeam = Team(cityName: homeTeamCity!, teamName: homeTeamName!, shortName: shortHomeTeam!.uppercaseString)
-                
-                let game = Game(league: league!, awayTeam: awayTeam, homeTeam: homeTeam)
-                
-                let isPlaying: Int? = jsonDict["isPlaying"] as? Int
-                
-                if isPlaying == nil {
-                    return nil
-                }
-                
-                game.playing = (isPlaying > 0) ? true : false
-                
-                if let awayTeamScore = jsonDict["awayScore"] as? String {
-                    if let scoreIntValue = awayTeamScore.toInt() {
-                        game.awayTeamScore = scoreIntValue
-                    }
-                }
-
-                if let homeTeamScore = jsonDict["homeScore"] as? String {
-                    if let scoreIntValue = homeTeamScore.toInt() {
-                        game.homeTeamScore = scoreIntValue
-                    }
-                }
-                
-                if let period: String? = jsonDict["period"] as? String {
-                    game.period = period
-                }
-                
-                return game
-                
-            }
             
             if let errorOccurred = error {
                 completionClosure(Game[](), error)
@@ -106,7 +52,7 @@ class ScoreManager: NSObject, NSURLSessionDelegate {
                     let receivedGamesArray: NSArray = jsonObject!["scores"] as NSArray
                     for gameDict: AnyObject in receivedGamesArray {
                         if gameDict as? NSDictionary {
-                            if let game = generateGame(gameDict as NSDictionary) {
+                            if let game = Game.gameWithJSON(gameDict as NSDictionary) {
                                 gameArray.append(game)
                             }
                         }
